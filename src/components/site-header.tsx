9a807153195980498846f635e4b07e9c177eb425
Anurag Rudra <anuragrudra81@gmail.com>
2025-12-06 10:47:06 +0000

    from the dropdown remove move in move out

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -31,7 +31,6 @@ const serviceLinks = [
   { href: "/services/office-cleaning", label: "office cleaning" },
   { href: "/services/construction-cleaning", label: "construction cleaning" },
   { href: "/services/childcare-cleaning", label: "childcare cleaning" },
-  { href: "/services/move-in-out", label: "move-in/out" },
 ];
 
 const mainNavLinks = [
