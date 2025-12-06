b8cf0eedd783ba00ecb91c89fe3259815af53904
Anurag Rudra <anuragrudra81@gmail.com>
2025-12-06 10:15:23 +0000

    the dropdown wallpaper faded with backgroud of the page

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -93,7 +93,7 @@ export function SiteHeader() {
                     <DropdownMenuTrigger className={`flex items-center gap-1 ${linkClassName}`} onMouseEnter={() => setIsServicesMenuOpen(true)}>
                         all services <ChevronDown className="h-4 w-4" />
                     </DropdownMenuTrigger>
-                    <DropdownMenuContent>
+                    <DropdownMenuContent className="bg-background">
                         {serviceLinks.map((link) => (
                         <DropdownMenuItem key={link.href} asChild>
                             <Link href={link.href} prefetch={false}>
